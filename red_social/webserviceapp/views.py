f22723d977470500c77207b88840aa578ad63e0a
isma <icabanasl22@fpcoruna.afundacion.org>
2024-05-16 11:40:22 +0200

    endpoint1

--- red_social/webserviceapp/views.py
+++ red_social/webserviceapp/views.py
@@ -7,4 +7,39 @@ from .models import Aficionados, Equipos, Contenido, Comentarios
 import json
 
 @csrf_exempt
+def crear_aficionado(request):
+    if request.method == 'POST':
+        try:
+            # Recuperar los datos de la solicitud de crear usuario
+            data = json.loads(request.body)
 
+            # Verificar si ya existe un usuario con el mismo email
+            if Usuario.objects.filter(email=data['email']).exists():
+                return HttpResponseConflict('Ya existe un usuario con ese email', content_type='text/plain')
+
+            # Crear un nuevo aficionado
+            with transaction.atomic():
+                nuevo_aficionado = Usuario(
+                    id_aficionado=data['id_aficionado'],
+                    username=data['username'],
+                    password=make_password(data['password']),
+                    email=data['email'],
+                    birthdate=data['birthdate'],
+                    Token_Sesion='',
+                    url_avatar=data['url_avatar']
+                )
+                nuevo_aficionado.save()
+
+            return JsonResponse({'mensaje': 'Aficionado creado exitosamente'}, status=201)
+
+        except KeyError:
+            # Faltan parámetros en la solicitud
+            return HttpResponseBadRequest('Faltan parámetros o son incorrectos', content_type='text/plain')
+
+        except Exception as e:
+            # Otros errores
+            return JsonResponse({'error': str(e)}, status=400)
+
+    else:
+        # Método no permitido
+        return JsonResponse({'error': 'Método no permitido'}, status=405)
